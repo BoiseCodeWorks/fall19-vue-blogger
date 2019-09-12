d612006d08ab0add6c03bd197d01ace2408128e2
Darryl Kilzer <darrylkilzer@gmail.com>
2019-09-11 19:11:35 -0600

    starting point

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container-fluid">
+      <div class="row p-3 bg-light shadow mb-2 justify-content-center text-right">
+        <div class="col-12">
+          <p class="m-0">Welcome {{user.name}}</p>
+        </div>
+        <div id="nav">
+          <router-link to="/">Home</router-link>|
+          <router-link to="/about">About</router-link>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
+<script>
+export default {
+  computed: {
+    user() {
+      return this.$store.state.user;
+    }
+  },
+  components: {}
+};
+</script>
+
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
+  position: absolute;
 }
 
 #nav a {
--- src/store.js
+++ src/store.js
@@ -1,16 +1,57 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import router from './router'
+import AuthService from './AuthService'
 
 Vue.use(Vuex)
 
+function getBaseState() {
+  return {
+    user: {}
+  }
+}
+
 export default new Vuex.Store({
-  state: {
-
-  },
+  state: getBaseState(),
   mutations: {
-
+    setUser(state, user) {
+      state.user = user
+    },
+    resetState(state) {
+      state = getBaseState()
+    }
   },
   actions: {
+    //#region -- AUTH STUFF --
+    async register({ commit, dispatch }, creds) {
+      try {
+        let user = await AuthService.Register(creds)
+        commit('setUser', user)
+        router.push({ name: "home" })
+      } catch (e) {
+        console.warn(e.message)
+      }
+    },
+    async login({ commit, dispatch }, creds) {
+      try {
+        let user = await AuthService.Login(creds)
+        commit('setUser', user)
+        router.push({ name: "home" })
+      } catch (e) {
+        console.warn(e.message)
+      }
+    },
+    async logout({ commit, dispatch }) {
+      try {
+        let success = await AuthService.Logout()
+        if (!success) { }
+        commit('resetState')
+        router.push({ name: "login" })
+      } catch (e) {
+        console.warn(e.message)
+      }
+    },
+    //#endregion
 
   }
 })
